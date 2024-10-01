720653b252601c6818a949038fa7c103dd0b42f5
josue agboton <josueagbotonking@gmail.com>
2024-10-01 17:31:04 +0100

    Activation of CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+/* eslint-disable prettier/prettier */
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Activer CORS
+  app.enableCors({
+    origin: "http://localhost:5173", // Autoriser l'origine spécifique de votre front-end
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Méthodes HTTP autorisées
+    credentials: true, // Activer l'envoi des cookies si nécessaire
+  });
   await app.listen(3000);
 }
 bootstrap();
